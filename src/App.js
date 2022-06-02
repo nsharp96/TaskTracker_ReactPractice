ea16049ce1c7d339ead88e944ac8d1713192aa67
nsharp96 <62970403+nsharp96@users.noreply.github.com>
2022-06-02 01:02:27 +0100

    Delete Task and change reminder

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from "react"
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers'
+
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        remind: true,
+    },
+    {
+        id: 2,
+        text: 'Pick up children',
+        day: 'Feb 6th at 3:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 4:30pm',
+        reminder: false,
+    }
+])
+
+//Function to delete task
+const deleteTask = (id) =>
+{
+  setTasks(tasks.filter( (task) => task.id !== id ))
+}
+
+//Toggle Reminder
+const toggleReminder = (id) =>
+{
+  setTasks(tasks.map( (task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+}
+
+  return (
+    <div className="container">
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to show'}
+    </div>
+  );
+}
+
+export default App;
